25fec3899cf63d98e7808e81aee547137f219456
SANTINO ADOBATTO BERGMAN <49761294@ort>
2026-08-19 08:40:27 -0300

    arreglo program.cs bugeado

--- Program.cs
+++ Program.cs
@@ -4,18 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
 var app = builder.Build();
-
-// Initialize database.
-Database.Initialize();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
